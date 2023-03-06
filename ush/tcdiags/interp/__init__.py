927f4f53b5b1d79a70fb3b56b672509875134eee
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-05 20:00:17 -0700

    Bringing branch UTD with WIP.

--- ush/tcdiags/interp/__init__.py
+++ ush/tcdiags/interp/__init__.py
@@ -61,72 +61,88 @@ def interp_ll2ra(varin: numpy.array, lats: numpy.array,
 
     # Compute the radial distance relative to the specified
     # geographical coordinate location.
-    rho = numpy.array([haversine((lats[idx], lons[idx]), (lat_0, lon_0))
+    fix = (lat_0, lon_0)
+    rho = numpy.array([haversine(fix, (lats[idx], lons[idx]))
                        for idx in range(len(lats))])
+
+    xx = numpy.array([haversine(fix, (lat_0, lons[idx]))
+                      for idx in range(len(lats))])
+    xx = numpy.where(lons < lon_0, -1.0*xx, xx)
+
+    yy = numpy.array([haversine(fix, (lats[idx], lon_0))
+                      for idx in range(len(lats))])
+    yy = numpy.where(lats < lat_0, -1.0*yy, yy)
+
+    phi = numpy.arctan2(yy, xx)
 
     # Define the grid attributes relative to the specified
     # geographical coordinate location.
     xlocs = numpy.where(numpy.logical_and(rho >= 0.0, rho <= max_radius))[0]
-    xrho = numpy.array([rho[idx] for idx in xlocs])
     xlats = numpy.array([lats[idx] for idx in xlocs])
     xlons = numpy.array([lons[idx] for idx in xlocs])
+    xrho = numpy.array([rho[idx] for idx in xlocs])
+    xphi = numpy.array([phi[idx] for idx in xlocs])
+    xvar = numpy.array([varin[idx] for idx in xlocs])
 
-    xx = numpy.array([haversine((lat_0, lon_0), (lat_0, lons[idx]))
-                      for idx in xlocs])
-    xx = numpy.where(xlons < lon_0, -1.0*xx, xx)
 
-    yy = numpy.array([haversine((lat_0, lon_0), (lats[idx], lon_0))
-                      for idx in xlocs])
-    yy = numpy.where(xlats < lat_0, -1.0*yy, yy)
+#    xx = numpy.array([haversine(fix, (lat_0, lons[idx]))
+#                      for idx in xlocs])
+#    xx = numpy.where(xlons < lon_0, -1.0*xx, xx)
+
+#    yy = numpy.array([haversine(fix, (lats[idx], lon_0))
+#                      for idx in xlocs])
+#    yy = numpy.where(xlats < lat_0, -1.0*yy, yy)
 
     # Compute the azimuthal angle relative to the specified
     # geographical coordinate location.
-    xphi = numpy.arctan2(yy, xx)
+#    xphi = numpy.arctan2(yy, xx)
+    dphi = numpy.radians(dphi)
 
     # Interpolate the Cartesian grid to the defined polar coordinate
     # grid.
-    var = numpy.empty()  # xrho.shape[0], xphi.shape[0]))
-    var[:] = numpy.nan
+    radius = numpy.arange(0.0, (max_radius + drho), drho)
+    azimuth = numpy.arange(-1.0*numpy.pi, (numpy.pi+dphi), dphi)
 
-    dphi = numpy.radians(dphi)
-    radius = drho
-    idx = 0
+    var = []
 
-    while(radius <= max(xrho)):
+    for ridx in range(len(radius)):
+        radii = radius[ridx]
 
-        radians = numpy.ndarray.tolist(numpy.where(numpy.logical_and(
-            xrho >= radius, xrho <= radius + drho))[0])
+        for aidx in range(len(azimuth)):
+            theta = azimuth[aidx]
+            array = []
 
-        phi = -1.0*numpy.pi
-        while(phi <= numpy.pi):
+            idxs = numpy.ndarray.tolist(numpy.where(rho >= radii)[0])
+            for idx in idxs:
 
-            phis = numpy.ndarray.tolist(numpy.where(numpy.logical_and(
-                xphi >= phi, xphi <= (phi + dphi)))[0])
+                if rho[idx] < (radii + drho):
+                    if (phi[idx] >= theta) and (phi[idx] < (theta+dphi)):
+                        array.append(varin[idx])
 
-            idxs = list(set(radians) & set(phis))
+            if len(array) == 0:
+                var.append(numpy.nan)
 
-            var[idx] = numpy.mean(varin[idxs])
-
-            phi = phi + dphi
-            idx = idx + 1
-
-        radius = radius + drho
+            else:
+                var.append(numpy.nanmean(array))
 
     var = numpy.array(var)
-    check = -1.0*numpy.isnan(var)
+
+    check = numpy.logical_not(numpy.isnan(var))
 
     xp = check.ravel().nonzero()[0]
-    fp = var[-1.0*numpy.isnan(var)]
+    fp = var[numpy.logical_not(numpy.isnan(var))]
 
-    x = numpy.isnan(var).rave().nonzero[0]
+    x = numpy.isnan(var).ravel().nonzero()[0]
 
     var[numpy.isnan(var)] = numpy.interp(x, xp, fp)
 
-    (nrho, nphi) = [len(xrho), len(xphi)]
+    (nrho, nphi) = [len(radius), len(azimuth)]
 
-    var = numpy.array(var).reshape((nrho, nphi))
+    varout = numpy.array(var).reshape((nrho, nphi))
 
-    return (xrho, xphi, var)
+    print(varout.min(), varout.max())
+
+    return (radius, azimuth, varout)
 
 
 # ----
