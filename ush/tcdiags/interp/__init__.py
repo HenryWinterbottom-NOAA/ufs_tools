b6887be9b126832a901033a3211ac9c3fc63f614
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-07 15:21:40 -0700

    Polar projection interpolation function.

--- ush/tcdiags/interp/__init__.py
+++ ush/tcdiags/interp/__init__.py
@@ -18,23 +18,70 @@
 # =========================================================================
 
 """
+Module
+------
 
+    __init__.py
 
+Description
+-----------
+
+    This module functions to handle various interpolation types.
+
+Functions
+---------
+
+    interp_ll2ra(varin, lats, lons, lat_0, lon_0, max_radius, drho,
+                 dphi)
+
+        This function interpolates a 2-dimensional variable, defined
+        on a Cartesian type grid, to a polar projection grid defined
+        by the attributes for the polar projection specified upon
+        entry.
+
+    interp_vertical(varin, zarr, levs)
+
+        This method interpolates a 3-dimensional variable to specified
+        vertical levels.
+
+Requirements
+------------
+
+- ufs_pytils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
+
+Author(s)
+---------
+
+    Henry R. Winterbottom; 07 March 2023
+
+History
+-------
+
+    2023-03-07: Henry Winterbottom -- Initial implementation.
 
 """
 
 # ----
 
-from math import asin, cos, radians, sin
+# pylint: disable=invalid-name
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+
+# ----
+
 from typing import List
 
-# from scipy.spatial import KDTree
-
 import numpy
-from tcdiags.geomets import haversine
 from exceptions import TCDiagsError
+from tcdiags.geomets import haversine
+from tools import parser_interface
 from utils.logger_interface import Logger
-# from wrf import interplevel
+from wrf import interplevel
+
+# ----
+
+# Define all available functions.
+__all__ = ["interp_ll2ra", "interp_vertical"]
 
 # ----
 
@@ -49,74 +96,136 @@ __email__ = "henry.winterbottom@noaa.gov"
 # ----
 
 
-def interp_ll2ra(varin: numpy.array, lats: numpy.array,
-                 lons: numpy.array, lon_0: float, lat_0: float,
-                 max_radius: float, drho: float, dphi: float) -> numpy.array:
-    """ """
+def interp_ll2ra(
+    varin: numpy.array,
+    lats: numpy.array,
+    lons: numpy.array,
+    lat_0: float,
+    lon_0: float,
+    max_radius: float,
+    drho: float,
+    dphi: float,
+) -> object:
+    """
+    Description
+    -----------
+
+    This function interpolates a 2-dimensional variable, defined on a
+    Cartesian type grid, to a polar projection grid defined by the
+    attributes for the polar projection specified upon entry.
+
+    Parameters
+    ----------
+
+    varin: array-type
+
+        A Python array-type variable containing the 2-dimensional
+        variable defined along the respective Cartesian grid.
+
+    lats: array-type
+
+        A Python array-type variable containing the 2-dimensional grid
+        of latitude coordinate values; the coordinate values are
+        assumed order south to north; units are degrees.
+
+    lons: array-type
+
+        A Python array-type variable containing the 2-dimensional grid
+        of longitude coordinate values; the coordinate values are
+        assumed to be within in the range[-180.0 to 180.0]; units are
+        degrees.
+
+    lat_0: float
+
+        A Python float value defining the reference latitude
+        coordinate value from which the polar grid projection will be
+        defined; units are degrees.
+
+    lon_0: float
+
+        A Python float value defining the reference longitude
+        coordinate value from which the polar grid projection will be
+        defined; the coordinate values are assumed to be within in the
+        range[-180.0 to 180.0]; units are degrees;
+
+    max_radius: float
+
+        A Python float value defining the maximum radial distance for
+        which to define the polar grid projection; units are meters.
+
+    drho: float
+
+        A Python float variable defining the radial distance interval
+        for the polar projection; units are meters.
+
+    dphi: float
+
+        A Python float value defining the aximuthal interval for the
+        polar projection; units are degrees.
+
+    Returns
+    -------
+
+    varout_obj: object
+
+        A Python object containing the interpolated variable as well
+        as the attributes of the polar projection.
+
+    """
 
     # Initialize the coordinate arrays.
     varin = numpy.ravel(varin)
     lats = numpy.ravel(lats)
     lons = numpy.ravel(lons)
+    dphi = numpy.radians(dphi)
 
     # Compute the radial distance relative to the specified
     # geographical coordinate location.
     fix = (lat_0, lon_0)
-    rho = numpy.array([haversine(fix, (lats[idx], lons[idx]))
-                       for idx in range(len(lats))])
+    rho = numpy.array(
+        [haversine(fix, (lats[idx], lons[idx])) for idx in range(len(lats))]
+    )
 
-    xx = numpy.array([haversine(fix, (lat_0, lons[idx]))
-                      for idx in range(len(lats))])
-    xx = numpy.where(lons < lon_0, -1.0*xx, xx)
+    xx = numpy.array([haversine(fix, (lat_0, lons[idx])) for idx in range(len(lats))])
+    xx = numpy.where(lons < lon_0, -1.0 * xx, xx)
 
-    yy = numpy.array([haversine(fix, (lats[idx], lon_0))
-                      for idx in range(len(lats))])
-    yy = numpy.where(lats < lat_0, -1.0*yy, yy)
+    yy = numpy.array([haversine(fix, (lats[idx], lon_0)) for idx in range(len(lats))])
+    yy = numpy.where(lats < lat_0, -1.0 * yy, yy)
 
     phi = numpy.arctan2(yy, xx)
 
-    # Define the grid attributes relative to the specified
-    # geographical coordinate location.
-    xlocs = numpy.where(numpy.logical_and(rho >= 0.0, rho <= max_radius))[0]
-    xlats = numpy.array([lats[idx] for idx in xlocs])
-    xlons = numpy.array([lons[idx] for idx in xlocs])
-    xrho = numpy.array([rho[idx] for idx in xlocs])
-    xphi = numpy.array([phi[idx] for idx in xlocs])
-    xvar = numpy.array([varin[idx] for idx in xlocs])
-
-
-#    xx = numpy.array([haversine(fix, (lat_0, lons[idx]))
-#                      for idx in xlocs])
-#    xx = numpy.where(xlons < lon_0, -1.0*xx, xx)
-
-#    yy = numpy.array([haversine(fix, (lats[idx], lon_0))
-#                      for idx in xlocs])
-#    yy = numpy.where(xlats < lat_0, -1.0*yy, yy)
-
-    # Compute the azimuthal angle relative to the specified
-    # geographical coordinate location.
-#    xphi = numpy.arctan2(yy, xx)
-    dphi = numpy.radians(dphi)
-
     # Interpolate the Cartesian grid to the defined polar coordinate
     # grid.
-    radius = numpy.arange(0.0, (max_radius + drho), drho)
-    azimuth = numpy.arange(-1.0*numpy.pi, (numpy.pi+dphi), dphi)
+    radial = numpy.arange(0.0, (max_radius + drho), drho)
+    azimuth = numpy.arange(-1.0 * numpy.pi, (numpy.pi + dphi), dphi)
 
+    msg = (
+        f"Defining polar projection grid of resolution {drho} meters "
+        f"and {dphi} degrees centered at longitude coordinate {lon_0} "
+        f"and latitude coordinate location {lat_0}."
+    )
+    logger.info(msg=msg)
+
+    # Interpolate the variable defined on the Cartesian grid to the
+    # established the radial and azimuthal angle coordinates; proceed
+    # accordingly.
     var = []
 
-    for ridx in range(len(radius)):
-        radii = radius[ridx]
+    # Define the radial coordinate.
+    for ridx in enumerate(radial):
+        radii = radial[ridx[0]]
 
-        for aidx in range(len(azimuth)):
-            theta = azimuth[aidx]
+        # Define the azimuthal coordinate.
+        for aidx in enumerate(azimuth):
+            theta = azimuth[aidx[0]]
             array = []
 
+            # Determine all (if any) input variable values within the
+            # radii and azimuth (theta) interval; proceed accordingly.
             idxs = numpy.ndarray.tolist(numpy.where(rho >= radii)[0])
             for idx in idxs:
-
                 if rho[idx] < (radii + drho):
-                    if (phi[idx] >= theta) and (phi[idx] < (theta+dphi)):
+                    if (phi[idx] >= theta) and (phi[idx] < (theta + dphi)):
                         array.append(varin[idx])
 
             if len(array) == 0:
@@ -125,31 +234,51 @@ def interp_ll2ra(varin: numpy.array, lats: numpy.array,
             else:
                 var.append(numpy.nanmean(array))
 
-    var = numpy.array(var)
+    # Interpolate in order to fill and missing (i.e., NaN) values.
+    interp_var = numpy.array(var)
 
-    check = numpy.logical_not(numpy.isnan(var))
-
+    check = numpy.logical_not(numpy.isnan(interp_var))
     xp = check.ravel().nonzero()[0]
-    fp = var[numpy.logical_not(numpy.isnan(var))]
+    fp = interp_var[numpy.logical_not(numpy.isnan(interp_var))]
+    x = numpy.isnan(interp_var).ravel().nonzero()[0]
 
-    x = numpy.isnan(var).ravel().nonzero()[0]
+    interp_var[numpy.isnan(var)] = numpy.interp(x, xp, fp)
+    (nrho, nphi) = [len(radial), len(azimuth)]
 
-    var[numpy.isnan(var)] = numpy.interp(x, xp, fp)
+    # Define and build the output variable object attributes.
+    varout_dict = {
+        "azimuth": azimuth,
+        "dphi": dphi,
+        "drho": drho,
+        "lat_0": lat_0,
+        "lon_0": lon_0,
+        "max_radius": max_radius,
+        "nphi": nphi,
+        "nrho": nrho,
+        "radial": radial,
+        "varout": numpy.array(interp_var).reshape((nrho, nphi)),
+    }
 
-    (nrho, nphi) = [len(radius), len(azimuth)]
+    msg = f"Output variable has radial dimension {nrho} and azimuthal dimension {nphi}."
+    logger.info(msg=msg)
 
-    varout = numpy.array(var).reshape((nrho, nphi))
+    varout_obj = parser_interface.object_define()
 
-    print(varout.min(), varout.max())
+    for varout_attr in varout_dict:
+        value = parser_interface.dict_key_value(
+            dict_in=varout_dict, key=varout_attr, no_split=True
+        )
+        varout_obj = parser_interface.object_setattr(
+            object_in=varout_obj, key=varout_attr, value=value
+        )
 
-    return (radius, azimuth, varout)
+    return varout_obj
 
 
 # ----
 
 
-def interp_vertical(varin: numpy.array, pres: numpy.array,
-                    levs: List) -> numpy.array:
+def interp_vertical(varin: numpy.array, zarr: numpy.array, levs: List) -> numpy.array:
     """
     Description
     -----------
@@ -200,6 +329,6 @@ def interp_vertical(varin: numpy.array, pres: numpy.array,
 
     except Exception as errmsg:
         msg = f"The vertical interpolation failed with error {errmsg}. Aborting!!!"
-        raise TCDiagsError(msg=msg)
+        raise TCDiagsError(msg=msg) from errmsg
 
     return varout
