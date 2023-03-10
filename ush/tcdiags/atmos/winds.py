0b5a94322286107cea8ababe28cb71c83f7a159a
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-10 09:20:34 -0700

    Added total wind field partitioning.

--- ush/tcdiags/atmos/winds.py
+++ ush/tcdiags/atmos/winds.py
@@ -58,10 +58,18 @@ Functions
         This function computes the global vorticity field using
         spherical harmonic transforms.
 
+    global_wind_part(inputs_obj)
+
+        This function computes the components of the total wind field
+        using spherical harmonic transforms; the methodology follows
+        from Lynch [1988].
+
 Requirements
 ------------
 
 - metpy; https://unidata.github.io/MetPy/latest/index.html
+
+- pyspharm; https://github.com/jswhit/pyspharm
 
 - ufs_pytils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
 
@@ -80,16 +88,17 @@ History
 # ----
 
 # pylint: disable=invalid-name
+# pylint: disable=too-many-locals
 
 # ----
 
 import gc
 from typing import Tuple, Union
 
-from metpy.units import units
 import numpy
 import spharm
 from exceptions import TCDiagsError
+from metpy.units import units
 from pint import Quantity
 from tools import parser_interface
 from utils.logger_interface import Logger
@@ -97,7 +106,7 @@ from utils.logger_interface import Logger
 # ----
 
 # Define all available functions.
-__all__ = ["global_divg", "global_vort"]
+__all__ = ["global_divg", "global_vort", "global_wind_part"]
 
 # ----
 
@@ -265,7 +274,7 @@ def global_divg(inputs_obj: object) -> object:
     inputs_obj: object
 
         A Python object updated to now contain the global divergence
-        field.
+        field `divg`.
 
     """
 
@@ -275,16 +284,27 @@ def global_divg(inputs_obj: object) -> object:
     nlevs = xdivg.shape[0]
 
     # Compute the divergence field; proceed accordingly.
+    msg = f"Computing global diverenge array of dimension {xdivg.shape}."
+    logger.info(msg=msg)
+
     for lev in range(nlevs):
         (u, v) = _get_lev_uv(inputs_obj=inputs_obj, lev=lev)
 
         (_, dataspec) = xspharm.getvrtdivspec(ugrid=u, vgrid=v)
         xdivg[lev, :, :] = xspharm.spectogrd(dataspec=dataspec)
 
+    # Define the correct units with respect to the input variable.
     xdivg = units.Quantity(xdivg, "1 / second")
+
     inputs_obj = parser_interface.object_setattr(
         object_in=inputs_obj, key="divg", value=xdivg
     )
+
+    msg = (
+        f"Global divergence values range({numpy.array(xdivg).min()}, "
+        f"{numpy.array(xdivg).max()}) {xdivg.units}."
+    )
+    logger.debug(msg=msg)
 
     # Deallocate memory for the spherical harmonic transform object.
     _cleanup(xspharm=xspharm)
@@ -317,7 +337,7 @@ def global_vort(inputs_obj: object) -> object:
     inputs_obj: object
 
         A Python object updated to now contain the global vorticity
-        field.
+        field `vort`.
 
     """
 
@@ -326,6 +346,9 @@ def global_vort(inputs_obj: object) -> object:
     xspharm = _init_spharm(array_in=xvort)
     nlevs = xvort.shape[0]
 
+    msg = f"Computing global vorticity array of dimension {xvort.shape}."
+    logger.info(msg=msg)
+
     # Compute the vorticity field; proceed accordingly.
     for lev in range(nlevs):
         (u, v) = _get_lev_uv(inputs_obj=inputs_obj, lev=lev)
@@ -333,10 +356,118 @@ def global_vort(inputs_obj: object) -> object:
         (dataspec, _) = xspharm.getvrtdivspec(ugrid=u, vgrid=v)
         xvort[lev, :, :] = xspharm.spectogrd(dataspec=dataspec)
 
+    # Define the correct units with respect to the input variable.
     xvort = units.Quantity(xvort, "1 / second")
+
     inputs_obj = parser_interface.object_setattr(
         object_in=inputs_obj, key="vort", value=xvort
     )
+
+    msg = (
+        f"Global vorticity values range({numpy.array(xvort).min()}, "
+        f"{numpy.array(xvort).max()}) {xvort.units}."
+    )
+    logger.debug(msg=msg)
+
+    # Deallocate memory for the spherical harmonic transform object.
+    _cleanup(xspharm=xspharm)
+
+    return inputs_obj
+
+
+# ----
+
+
+def global_wind_part(inputs_obj: object) -> object:
+    """
+    Description
+    -----------
+
+    This function computes the components of the total wind field
+    using spherical harmonic transforms; the methodology follows from
+    Lynch [1988].
+
+    Parameters
+    ----------
+
+    inputs_obj: object
+
+        A Python object containing, at minimum, the zonal and
+        meridional wind components; units are meters per second.
+
+    Returns
+    -------
+
+    inputs_obj: object
+
+        A Python object updated to now contain the global total wind
+        field paritioning; the respective quantities are as follows.
+
+        - (udiv, vdiv); the components of the divergent component of
+          the global total wind field.
+
+        - (uhrm, vhrm); the components of the harmonic (i.e.,
+          residual) component of the global total wind field.
+
+        - (uvor, vvor); the components of the rotational component of
+          the global total wind field.
+
+    References
+    ----------
+
+    Lynch, P., 1994: Paritioning the Wind in a Limited
+    Domain. Mon. Wea. Rev., 116, 86-93.
+
+    https://doi.org/10.1175/1520-0493(1988)116<0086:DTWFVA>2.0.CO;2
+
+    """
+
+    # Initialize the local variable and objects.
+    (xudiv, xuhrm, xuvor, xvdiv, xvhrm, xvvor) = [
+        numpy.zeros(inputs_obj.uwnd.shape) for idx in range(6)
+    ]
+
+    xspharm = _init_spharm(array_in=xuvor)
+    nlevs = xuvor.shape[0]
+
+    msg = f"Computing global partitioned total wind arrays of dimension {xuvor.shape}."
+    logger.info(msg=msg)
+
+    # Compute the total wind components; proceed accordingly.
+    for lev in range(nlevs):
+        (u, v) = _get_lev_uv(inputs_obj=inputs_obj, lev=lev)
+        (zspec_save, dspec_save) = xspharm.getvrtdivspec(ugrid=u, vgrid=v)
+
+        # Compute the divergent component of the total wind field.
+        (zspec, dspec) = [numpy.zeros(zspec_save.shape), dspec_save]
+        (xudiv[lev, :, :], xvdiv[lev, :, :]) = xspharm.getuv(
+            vrtspec=zspec, divspec=dspec
+        )
+
+        # Compute the rotational component of the total wind field.
+        (zspec, dspec) = [zspec_save, numpy.zeros(dspec_save.shape)]
+        (xuvor[lev, :, :], xvvor[lev, :, :]) = xspharm.getuv(
+            vrtspec=zspec, divspec=dspec
+        )
+
+        # Compute the residual (i.e., harmonic) component of the total
+        # wind field.
+        xuhrm[lev, :, :] = numpy.array(
+            u[:, :]) - (xuvor[lev, :, :] + xudiv[lev, :, :])
+        xvhrm[lev, :, :] = numpy.array(
+            v[:, :]) - (xvvor[lev, :, :] + xvdiv[lev, :, :])
+
+    # Define the correct units with respect to the input variable.
+    (xudiv, xuhrm, xuvor, xvdiv, xvhrm, xvvor) = [
+        inputs_obj.uwnd.units for wcmpn in [xudiv, xuhrm, xuvor, xvdiv, xvhrm, xvvor]
+    ]
+
+    inputs_obj = [
+        parser_interface.object_setattr(
+            object_in=inputs_obj, key=key, value=value)
+        for key in ["udiv", "uhrm", "uvor", "vdiv", "vhrm", "vvor"]
+        for value in [xudiv, xuhrm, xuvor, xvdiv, xvhrm, xvvor]
+    ]
 
     # Deallocate memory for the spherical harmonic transform object.
     _cleanup(xspharm=xspharm)
